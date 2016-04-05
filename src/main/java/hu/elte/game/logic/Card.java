52c9cfd43fd8b79324b204541213b5b3465587f5
Dago <bardi.zsolt.gabor@gmail.com>
2016-04-05 21:01:15 +0200

    Cards

---
+++ src/main/java/hu/elte/game/logic/Card.java
@@ -0,0 +1,27 @@
+package hu.elte.game.logic;
+
+public class Card {
+	private Integer money;
+	private Integer step;
+	private String text;
+	
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getStep() {
+		return step;
+	}
+	public void setStep(int step) {
+		this.step = step;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+}
---
+++ src/main/java/hu/elte/game/logic/Player.java
@@ -0,0 +1,5 @@
+package hu.elte.game.logic;
+
+public class Player {
+
+}
