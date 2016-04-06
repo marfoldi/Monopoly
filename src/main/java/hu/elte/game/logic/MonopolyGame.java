4f37815eaef7cc5dcc0923179c63b39b086a96d6
Daniel Torok <daniel.torok@sap.com>
2016-04-06 17:52:03 +0200

    [MonopolyGame] class, buildHouse, ownsAll
    Change-Id: Ic5626e791f16318ee415d0a2dded49b34fb2e1e4

---
+++ src/main/java/hu/elte/game/logic/MonopolyGame.java
@@ -0,0 +1,65 @@
+package hu.elte.game.logic;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class MonopolyGame {
+	private ArrayList<IField> table;
+	private ArrayList<Player> players;
+	private ArrayList<Card> chanceCards;
+	private ArrayList<Card> chestCards;
+	
+	private Player currentPlayer;
+	
+	public void buildHouse() {
+		IField field = table.get(currentPlayer.getPosition());
+		if (!field.getClass().equals(LandField.class)) {
+			// exception
+			return;
+		}
+		
+		LandField landField = (LandField) field;
+		if (!landField.getOwner().equals(currentPlayer)) {
+			// exception
+			return;
+		}
+		
+		if (!ownsAll(landField) || !(landField.getHousePrice() <= currentPlayer.getMoney())) {
+			// exception
+			return;
+		}
+		
+		if (!landField.buildHouse()) {
+			// exception
+			return;
+		}
+		
+		// Build successful
+		
+		if (!currentPlayer.decreaseWithHouse(landField)) {
+			// internal error
+			return;
+		}
+	}
+	
+	public void sellHouse() {
+		
+	}
+	
+	private boolean ownsAll(LandField landField) {
+		Stream<IField> fieldStream = table.stream();
+		Stream<IField> filteredStream = fieldStream.filter(field -> {
+			if (!field.getClass().equals(LandField.class)) {
+				return false;
+			}
+			
+			LandField currentField = (LandField) field;
+			return currentField.getCity().equals(landField.getCity());
+		});
+		
+		
+		return filteredStream.allMatch(field -> {
+			return ((LandField) field).getOwner().equals(currentPlayer);
+		});
+	}
+}
