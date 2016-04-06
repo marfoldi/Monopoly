41edc34c89dc246f8bf96bba5c9a2e0f37f78b81
Daniel Torok <daniel.torok@sap.com>
2016-04-06 17:51:09 +0200

    [Player] Player class, decreaseWith house / estate
    Change-Id: I8234d95dc81019c5de03f50930ee57dd64efc213

--- src/main/java/hu/elte/game/logic/Player.java
+++ src/main/java/hu/elte/game/logic/Player.java
@@ -1,5 +1,46 @@
 package hu.elte.game.logic;
 
 public class Player {
+	private final int START_MONEY = 70000;
+	
+	private String name;
+	private int position;
+	private int money;
+	
+	public Player(String name) {
+		this.name = name;
+		this.position = 0;
+		this.money = START_MONEY;
+	}
+	
+	public int getPosition() {
+		return this.position;
+	}
 
+	public int getMoney() {
+		return this.money;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public boolean decreaseWithHouse(LandField field) {
+		if (this.money < field.getHousePrice()) {
+			return false;
+		}
+		
+		this.money -= field.getHousePrice();
+		return true;
+	}
+	
+	public boolean decreaseWithEstate(PurchasableField field) {
+		if (this.money < field.getPrice()) {
+			return false;
+		}
+		
+		this.money -= field.getPrice();
+		return true;
+	}
+	
 }
