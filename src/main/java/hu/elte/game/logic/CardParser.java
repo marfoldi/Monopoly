eafd922832223331c5fd7c745ff449251d56b99f
Daniel Torok <daniel.torok@sap.com>
2016-04-06 13:37:33 +0200

    [Parsers] IField and Card
    Change-Id: Ib32c04f9a9a9a35ccd310af8402ef9571f8a3115

---
+++ src/main/java/hu/elte/game/logic/CardParser.java
@@ -0,0 +1,41 @@
+package hu.elte.game.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+/**
+ * Parses the Card objects from JSON
+ * @author I321357
+ *
+ */
+
+public class CardParser {
+	
+	/**
+	 * Parses the Card objects from the JSONArray
+	 * @param cards
+	 * @return
+	 * @throws JSONException
+	 */
+	public static List<Card> parseCards(JSONArray cards) {
+		List<Card> retVal = new ArrayList<Card>();
+		for (int i=0; i<cards.size(); i++) {
+			Card field = parseCardFromJSONObject(cards.getJSONObject(i));
+			retVal.add(field);
+		}
+		
+		return retVal;
+	}
+
+	private static Card parseCardFromJSONObject(JSONObject card) {
+		int money = card.getInt("money");
+		int step = card.getInt("step");
+		String text = card.getString("text");
+		
+		return new Card(money, step, text);
+	}
+
+}
---
+++ src/main/java/hu/elte/game/logic/FieldParser.java
@@ -0,0 +1,66 @@
+package hu.elte.game.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+/**
+ * Parses the IField objects from JSON
+ * @author I321357
+ *
+ */
+
+public class FieldParser {
+	
+	/**
+	 * Parses the IField objects from the JSONArray
+	 * @param fields
+	 * @return
+	 * @throws JSONException
+	 */
+	public static List<IField> parse(JSONArray fields) {
+		List<IField> retVal = new ArrayList<IField>();
+		for (int i=0; i<fields.size(); i++) {
+			IField field = parseFieldFromJSONObject(fields.getJSONObject(i));
+			retVal.add(field);
+		}
+		
+		return retVal;
+	}
+
+	private static IField parseFieldFromJSONObject(JSONObject field) {
+		String type = field.getString("type");
+		String name = field.getString("name");
+		
+		if ("Field".equals(type)) {
+			return new Field(name);
+		} else {
+			int price = field.getInt("price");
+			
+			if ("LandField".equals(type)) { 
+				return parseLandFieldFromJSONObject(field, name, price);
+			} else if ("PurchasableField".equals(type)) {
+				return parsePurchasableFieldFromJSONObject(field, name, price);
+			}
+		}
+		
+		return null;
+	}
+
+	private static IField parsePurchasableFieldFromJSONObject(JSONObject field, String name, int price) {
+		ArrayList<Integer> incomings = JSONParser.parseJSONArray(field.getJSONArray("incomings"));
+		
+		return new PurchasableField(name, price, incomings);
+	}
+
+	private static LandField parseLandFieldFromJSONObject(JSONObject field, String name, int price) {
+		String city = field.getString("city");
+		int housePrice = field.getInt("housePrice");
+		ArrayList<Integer> rentalFee = JSONParser.parseJSONArray(field.getJSONArray("rentalFee"));
+		
+		return new LandField(name, price, city, housePrice, rentalFee);
+	}
+	
+}
