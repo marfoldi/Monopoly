cc34dc0e6c47f8a8a0359b486b7f08f05d1a0ea0
Daniel Torok <daniel.torok@sap.com>
2016-04-14 10:08:35 +0200

    [FlowController] interface
    Change-Id: I368e0eb1a5a40ff5ba73c7b82a3271754f48d7a7

---
+++ src/main/java/hu/elte/game/logic/IFlowController.java
@@ -0,0 +1,41 @@
+package hu.elte.game.logic;
+
+import java.util.List;
+
+/**
+ * An implementation of this interface must be able to handle the turn based player switch analogy,
+ * and also communicate the taken actions between players.
+ *
+ */
+public interface IFlowController {
+	/**
+	 * Sets the listener which will be called to make the necessary changes in the controller - user interface
+	 * @param listener
+	 */
+	public void setControllerListener(IFlowControllerListener listener);
+	
+	/**
+	 * Sets the listener which will be called to make the necessary changes in the model
+	 * @param listener
+	 */
+	public void setModelListener(IFlowControllerListener listener);
+	
+	/**
+	 * Sets the players, the 'nextPlayer' will choose among these names
+	 * @param playerNames
+	 */
+	public void setPlayers(List<String> playerNames);
+	
+	/**
+	 * Call this when the player's turn is over, and the control must be transferred to the next player
+	 * @param changeSet (optional) the final actions the player took before handling over the control 
+	 */
+	public void nextPlayer(ChangeSet changeSet);
+	
+	/**
+	 * Call this when the player's took actions that requires other players to see it 
+	 * @param changeSet the actions took by the player
+	 */
+	public void sendChangeSet(ChangeSet changeSet);
+	
+}
---
+++ src/main/java/hu/elte/game/logic/IFlowControllerListener.java
@@ -0,0 +1,19 @@
+package hu.elte.game.logic;
+
+/**
+ * Listener used in the IFlowController
+ *
+ */
+public interface IFlowControllerListener {
+	/**
+	 * Called when the current player changes
+	 * @param playerName the now acting player's name
+	 */
+	public void onCurrentPlayerChange(String playerName);
+	
+	/**
+	 * Called when changes happened at a player, and they need to be displayed / stored this side
+	 * @param changeSet
+	 */
+	public void onGameStateChange(ChangeSet changeSet);
+}
